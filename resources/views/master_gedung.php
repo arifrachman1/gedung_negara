a909f492f4e3e9de0eda64e60521faf5e35f2644
bahtiar404 <kangilham6@gmail.com>
2020-10-12 14:23:02 +0700

    Front

---
+++ resources/views/master_gedung.php
@@ -0,0 +1,12 @@
+<?php
+include('header.php');
+?>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Master Gedung</h1>
+        </div>
+        <!-- /.container-fluid -->
+<?php
+include('footer.php');
+?>
