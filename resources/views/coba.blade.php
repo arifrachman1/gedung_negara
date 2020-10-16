542d0f7ae53fcf09f1e267608b7164ebd85d0e2b
bahtiar404 <kangilham6@gmail.com>
2020-10-16 11:02:27 +0700

    RevRole

--- resources/views/coba.blade.php
+++ resources/views/coba.blade.php
@@ -6,52 +6,48 @@
 <body>
 <br>
 <div class="container">
-  <div class="panel panel-default">
-    <div class="panel-heading">Form Tambah Data Dinamis</div>
-    <div class="panel-body">
-      <!-- membuat form  -->
-      <!-- gunakan tanda [] untuk menampung array  -->
-            <div class="control-group after-add-more">
-                    <label>Nama Komponen</label>
-                    <input type="text" name="nama[]" class="form-control">
-                    <label>Satuan</label>
-                    <input type="text" name="nama[]" class="form-control">
-                    <button class="btn btn-success add-more" type="button">
-                    <i class="glyphicon glyphicon-plus"></i> Add
-            </div>
-            <script type="text/javascript">
-                $(document).ready(function() {
-                $(".add-more").click(function(){ 
-                    var html = $(".copy").html();
-                    $(".after-add-more").after(html);
-                });
-
-                // saat tombol remove dklik control group akan dihapus 
-                $("body").on("click",".remove",function(){ 
-                    $(this).parents(".control-group").remove();
-                });
-                });
-            </script>
-            <button class="btn btn-success" type="submit">Submit</button>
-        </form>
-
-        <!-- class hide membuat form disembunyikan  -->
-        <!-- hide adalah fungsi bootstrap 3, klo bootstrap 4 pake invisible  -->
-        <div class="copy hide">
-            <div class="control-group">
-              <label>Subkomponen</label>
-              <input type="text" name="nama[]" class="form-control">
-              <label>Satuan</label>
-              <input type="text" name="nama[]" class="form-control">
-              <br>
-              <button class="btn btn-danger remove" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
-              <hr>
-            </div>
-          </div>
-        </div>
+<div class="container">
+      <div class="col-md-offset-5 col-md-5 check-row">
+                      <form role="form">
+                          <div class="form-group">   
+                          <div class="checkbox">
+                            <label>
+                              <input type="checkbox" class="check" id="checkAll" value="gedung"> Centang semua
+                            </label>
+                          </div>
+                          <div class="checkbox">
+                            <label>
+                            <input type="checkbox" class="check" value="gedung" > Membuat data baru
+                            </label>
+                          </div>
+                          <div class="checkbox">
+                            <label>
+                              <input type="checkbox" class="check" value="gedung" > Melihat data
+                            </label>
+                          </div>  
+                            <div class="checkbox">
+                            <label>
+                              <input type="checkbox" class="check" value="gedung"> Mengedit data
+                            </label>
+                          </div>
+                          <div class="checkbox">
+                            <label>
+                              <input type="checkbox" class="check" value="gedung"> Menghapus data
+                            </label>
+                          </div>
+                        </form>
+      </div>
     </div>
-  </div>
 </div>
+
+<script>
+$("#checkAll").click(function () {
+    $(".check").prop('checked', $(this).prop('checked'));
+});
+$("#checkAllS").click(function () {
+    $(".checkS").prop('checked', $(this).prop('checked'));
+});
+</script>
 <!-- fungsi javascript untuk menampilkan form dinamis  -->
 <!-- penjelasan :
 saat tombol add-more ditekan, maka akan memunculkan div dengan class copy -->
