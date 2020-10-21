6c94312fe71160bfb3aa04eabbab0070937c2aeb
AriefRachman99 <mariefrachmanm@gmail.com>
2020-10-21 07:15:28 +0700

    Master user selesai

---
+++ app/ModelHasRole.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelHasRole extends Model
+{
+    protected $table = 'model_has_roles';
+}
--- resources/views/User/master_user.blade.php
+++ resources/views/User/master_user.blade.php
@@ -28,7 +28,9 @@
                     <tr>
                       <th>Nama</th>
                       <th>Email</th>
+                      @can('user.delete')
                       <th>Action</th>
+                      @endcan
                     </tr>
                   </thead>
                   <tbody>
@@ -36,14 +38,16 @@
                     <tr>
                       <td>{{ $val->name }}</td>
                       <td>{{ $val->email }}</td>
-                      <td>
-                      @can('user.update')
-                      <a class="btn btn-warning mr-1" href="{{ url('edituser/'.$val->id) }}"><i class="button"><span class="icon text-white-100">Edit</span> </i></a> 
-                      @endcan
                       @can('user.delete')
-                      <a class="btn btn-danger" href="{{ url('hapususer/'.$val->id) }}"><i class="button"><span class="icon text-white-100">Hapus</span> </i></a>
+                        <td>
+                        @can('user.update')
+                        <a class="btn btn-warning mr-1" href="{{ url('edituser/'.$val->id) }}"><i class="button"><span class="icon text-white-100">Edit</span> </i></a> 
+                        @endcan
+                        @can('user.delete')
+                        <a class="btn btn-danger" href="{{ url('hapususer/'.$val->id) }}"><i class="button"><span class="icon text-white-100">Hapus</span> </i></a>
+                        @endcan
+                        </td>
                       @endcan
-                      </td>
                     </tr>
                   @endforeach
                   </tbody>
