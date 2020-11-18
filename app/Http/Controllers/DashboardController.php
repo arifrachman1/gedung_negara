1ab3ec80936ac3e7a291056e128503f7d91a8b62
Moh Khairul Anwar <48055398+mohkhairulanwar06031997@users.noreply.github.com>
2020-11-18 13:57:00 +0700

    dashboard kerusakan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Gedung;
 use App\User;
+use App\Kerusakan;
 use App\KategoriGedung;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,7 @@ class DashboardController extends Controller
         $user = User::count();
         $jenis = KategoriGedung::count();
         $gedung = Gedung::count();
-        return view('dashboard',compact('gedung','user','jenis'));
+        $kerusakan = Kerusakan::count();
+        return view('dashboard',compact('gedung','user','jenis','kerusakan'));
     }
 }
