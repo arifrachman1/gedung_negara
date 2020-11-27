0866d842d8d38a7e01271cd2b4c8ba086f95fd2f
Iksan Fityanto <iksanbayuaji@gmail.com>
2020-11-27 10:25:38 +0700

    ui

--- resources/views/Kerusakan/export_pdf_kerusakan.blade.php
+++ resources/views/Kerusakan/export_pdf_kerusakan.blade.php
@@ -25,37 +25,11 @@
                 border: 1px solid black;
             }
             body {
-<<<<<<< HEAD
-                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                    margin-top: 3cm;
-                    margin-left: 2cm;
-                    margin-right: 2cm;
-                    margin-bottom: 2cm;
-                }
-
-            #watermark {
-                position: fixed;
-                    /** 
-                        Set a position in the page for your image
-                        This should center it vertically
-                    **/
-                    bottom:   5cm;
-                    left:     11cm;
-
-                    /** Change image dimensions**/
-                    width:    8cm;
-                    height:   8cm;
-
-                    /** Your watermark should be behind every content**/
-                    z-index:  -1000;
-                }
-=======
                 margin-top: 3cm;
                 margin-left: 2cm;
                 margin-right: 2cm;
                 margin-bottom: 2cm;
             }           
->>>>>>> 1a626de43a638010e5ef5fae9c45fc9f726c8882
             @page { margin: 0cm 0cm; }
             .header {
                     position: fixed;
@@ -75,75 +49,6 @@
         </style>
     </head>
     <body>
-<<<<<<< HEAD
-        <div id="watermark">
-            <img src="{{ asset('style/img/watermark.png') }}" width="100%" height="100%"/>
-        </div>
-          <div class="header">
-          <img src="{{ asset('style/img/header.png') }}"  width="100%" height="100%"/>
-        </div>
-        <div class="footer">
-            Page <span class="pagenum"></span>
-        </div>  
- 
-	<table class='table table-center table-bordered'>
-		<thead>
-			<tr>
-				<th rowspan="2">No</th>
-                <th rowspan="2">Komponen</th>
-                <th rowspan="2">Sub Komponen</th>
-                <th rowspan="2">Satuan</th>
-                <th rowspan="2">Jumlah</th>
-                <th rowspan="2">Bobot</th>
-                <th colspan="10" colspan="5">Klasifikasi Kerusakan</th>
-                <th rowspan="2" colspan="3">Tingkat Kerusakan</th>
-			</tr>
-            <tr>
-                <th colspan="2">0.2</th>
-                <th colspan="2">0.4</th>
-                <th colspan="2">0.6</th>
-                <th colspan="2">0.8</th>
-                <th colspan="2">1.0</th>
-            </tr>
-            <tr>
-                <th>(1)</th>
-                <th>(2)</th>
-                <th>(3)</th>
-                <th>(4)</th>
-                <th>(5)</th>
-                <th>(6)</th>
-                <th colspan="2">(7)</th>
-                <th colspan="2">(8)</th>
-                <th colspan="2">(9)</th>
-                <th colspan="2">(10)</th>
-                <th colspan="2">(11)</th>
-                <th colspan="3">(12)</th>
-            </tr>
-        </thead>
-        @php $nomor = 1 @endphp
-        @foreach($komponens as $komponen)
-            @foreach($komponen->subKomponen as $subIndex => $subKomponen)
-            <tr>
-                <td>{{ $nomor++ }}</td>
-                @if($subIndex == 0)
-                    <td rowspan="{{ $komponen->numberOfSub}}">{{ $komponen->nama }}</td>
-                @endif
-                <td>{{ $subKomponen->nama }}</td>
-                <td>{{ $subKomponen->satuan }}</td>
-                <td>{{ $subKomponen->jumlah }}</td>
-                <td>{{ $subKomponen->bobot }}</td>
-                @if($subKomponen->id_satuan == 1)
-                    <td colspan="10">{{ ($subKomponen->nama_opsi) ? $subKomponen->nama_opsi : '-' }}</td>
-                @elseif($subKomponen->id_satuan == 2)
-                    @foreach($subKomponen->kerusakan_klasifikasi as $kk)
-                        <td>{{ $kk->nilai_input_klasifikasi }}</td>
-                        <td>{{ $kk->nilai_input_klasifikasi * $kk->klasifikasi }} %</td>
-                    @endforeach
-                @else
-                    @foreach($subKomponen->kerusakan_klasifikasi as $kk)
-                        <td>{{ $kk->nilai_input_klasifikasi }}</td>
-                        <td>{{ ($subKomponen->jumlah) ? (($kk->nilai_input_klasifikasi / $subKomponen->jumlah) * $kk->klasifikasi) : 0 }}</td>
-=======
         <div class="header">
             <img src="{{ asset('style/img/header.png') }}"  width="100%" height="100%"/>
         </div>        
@@ -278,7 +183,6 @@
                             <td rowspan="{{ $komponen->numberOfSub }}">{{ $komponen->sumTingkatKerusakanText }}</td>
                         @endif
                     </tr>
->>>>>>> 1a626de43a638010e5ef5fae9c45fc9f726c8882
                     @endforeach
                 @endforeach
                 <tr>
